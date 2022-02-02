31fd436157776b21e1af263b7b64d69b1520dc21
PaulV <pvollrath3@gatech.edu>
2022-02-02 17:06:26 -0500

    Add existing file

---
+++ Objects.py
@@ -0,0 +1,29 @@
+#Class for Objects in N-body Simulator
+
+class bodies:
+    def __init__(self,name,mass,iposition,ivelocity):
+        self.name = name
+        self.mass = mass
+        self.position = iposition
+        self.velocity = ivelocity
+        self.position_history = [iposition]
+        self.velocity_history = [ivelocity]
+        self.kinetic_energy = []
+
+    def get_position(self):
+        return self.position
+
+    def get_velocity(self):
+        return self.velocity
+
+    def get_mass(self):
+        return self.mass
+
+    def update_position(self,new_position):
+        self.position = new_position
+        self.position_history.append(new_position)
+
+    def update_velocity(self,new_velocity):
+        self.velocity = new_velocity
+        self.velocity_history.append(new_velocity)
+    
