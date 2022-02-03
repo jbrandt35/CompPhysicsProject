ca64ec1b368301772b75f1985052816214c56825
joshua.brandt <joshua.brandt@ligo.org>
2022-02-03 18:34:19 -0500

    Added JSON feature and changed Body object a little

---
+++ Main.py
@@ -0,0 +1,4 @@
+import Parser
+
+#Just a test for now until config file is done
+Planets = [Parser.parse_object("SimObjects/Earth.JSON")]
--- Objects.py
+++ Objects.py
@@ -1,14 +1,18 @@
-#Class for Objects in N-body Simulator
+import numpy as np
 
-class body:
+#Class for Objects in N-body Simulator
+class Body:
     def __init__(self,name,mass,iposition,ivelocity):
         self.name = name
         self.mass = mass
         self.position = iposition
         self.velocity = ivelocity
-        self.position_history = [iposition]
-        self.velocity_history = [ivelocity]
-        self.kinetic_energy = []
+        self.position_history = np.array([iposition])
+        self.velocity_history = np.array([ivelocity])
+        self.kinetic_energy = (1/2) * self.mass * np.linalg.norm(ivelocity)**2
+
+    def __repr__(self):
+        return str({"name":self.name, "mass":self.mass, "position":self.position, "velocity":self.velocity})
 
     def get_position(self):
         return self.position
@@ -21,9 +25,10 @@ class body:
 
     def update_position(self,new_position):
         self.position = new_position
-        self.position_history.append(new_position)
+        np.append(self.position_history, new_position)
 
     def update_velocity(self,new_velocity):
         self.velocity = new_velocity
-        self.velocity_history.append(new_velocity)
+        np.append(self.velocity_history, new_velocity)
+        self.kinetic_energy = (1/2) * self.mass * np.linalg.norm(new_velocity)**2
     
