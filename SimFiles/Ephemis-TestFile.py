f72ca7ccb498e5385079a732c9b3d9254b5774cd
joshua.brandt <joshua.brandt@ligo.org>
2022-03-28 18:24:20 -0400

    Ephemeris class to numpy array

--- SimFiles/Ephemis-TestFile.py
+++ SimFiles/Ephemis-TestFile.py
@@ -1,6 +1,7 @@
 from astropy.time import Time
 from astropy.coordinates import solar_system_ephemeris, EarthLocation
 from astropy.coordinates import get_body_barycentric_posvel, get_body
+import numpy as np
 
 #t is just the time we are initializing, and the line below sets what ephemeris the code will use. There are a couple different options for that
 t = Time("2022-04-27 23:22")
@@ -10,5 +11,6 @@ solar_system_ephemeris.set('jpl')
 earth = get_body_barycentric_posvel('earth',t)
 
 #first part prints the methods we can use on earth (note that there are a couple other functions we can use to get the position like just get_body)
-print(dir(earth))
-print(earth)
+EarthCoords = np.array(earth[0].get_xyz())
+print(EarthCoords)
+print(type(EarthCoords))
