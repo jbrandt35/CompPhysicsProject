2944ab9896439be5138b6d05850d5beb61e3e3f3
PaulV <pvollrath3@gatech.edu>
2022-03-28 19:28:27 -0400

    Ephemeris values added to Parser

--- Parser.py
+++ Parser.py
@@ -17,13 +17,30 @@ def parse_objects(files):
             print(f"Can't load {file}.")
 
         if data["mass"] == "<Find>":
-            if data["name"] == "Earth":
+            if data["name"] == "earth":
                 print("Earth's mass not provided, pulling from Astropy")
                 data["mass"] = constants.M_earth.value
-            elif data["name"] == "Sun":
+            elif data["name"] == "sun":
                 print("Sun's mass not provided, pulling from Astropy")
                 data["mass"] = constants.M_sun.value
 
+        if data["iposition"] == "<Find>":
+            #Find the bodies name
+            BodyName = data["name"]
+            #get tupe of position and velocity for the body
+            BodyInstance = get_body_barycentric_posvel(BodyName,t)
+            #get position array
+            BodyCoords = np.array(BodyInstance[0].get_xyz(),float)
+            data["iposition"] = BodyCoords * 10e3 #10e3 is added because ephemeris gives value in km
+
+        if data["ivelocity"] == "<Find>":
+            BodyName = data["name"]
+            BodyInstance = get_body_barycentric_posvel(BodyName,t)
+            #get velocity array
+            BodyVel = np.array(BodyInstance[1].get_xyz(),float)
+            data["ivelocity"] = BodyVel * 0.011574074 #float value is conversion factor from km/day to m/s
+
+        ###################################################################################
         body = Body(data["name"], data["mass"], data["iposition"], data["ivelocity"])
 
         print(f"Successfully parsed {body.name}")
